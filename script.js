390e0ad206f8e8ef9795573ff01c54a640e5270a
AnuragRocks <anurichking@gmail.com>
2025-01-03 17:45:32 +0530

    Clicking on windows icon will open the menu
    Added a functionality that only clicking on the windows icon will open the menu.
    Earlier clicking on the taskbar was opening the menu. Thus made it specific to the windows icon only.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let taskbar =document.getElementsByClassName('taskbar')[0]
 let startmenu =document.getElementsByClassName('startmenu')[0]
+let leftIcon = document.getElementsByClassName('left')[0]; // Selecting the left icon
 
 console.log("clicked");
-taskbar.addEventListener("click", () => {
+leftIcon.addEventListener("click", () => {
 
     if(startmenu.style.bottom == "50px" )
     {
